9ae0612796f5a996a4ff4ea38625625d883e57d1
Edwin Njeru <mailnjeru@gmail.com>
2022-02-01 15:24:31 +0300

    Integrate typeahead into the invoice entity

--- src/main/webapp/app/erp/erp-pages/payments/invoice/update/invoice-update.component.ts
+++ src/main/webapp/app/erp/erp-pages/payments/invoice/update/invoice-update.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HttpResponse } from '@angular/common/http';
 import { FormBuilder, Validators } from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
-import { Observable } from 'rxjs';
-import { finalize, map } from 'rxjs/operators';
+import { concat, Observable, of, Subject } from 'rxjs';
+import { catchError, debounceTime, distinctUntilChanged, filter, finalize, map, switchMap, tap } from 'rxjs/operators';
 
 import { InvoiceService } from '../../../../erp-common/services/invoice.service';
 import {Store} from "@ngrx/store";
@@ -17,6 +17,8 @@ import { IPlaceholder } from '../../../../erp-common/models/placeholder.model';
 import { PaymentLabelService } from '../../../../erp-common/services/payment-label.service';
 import { IInvoice, Invoice } from '../../../../erp-common/models/invoice.model';
 import { PlaceholderService } from '../../../../erp-common/services/placeholder.service';
+import { LabelSuggestionService } from '../../../../erp-common/suggestion/label-suggestion.service';
+import { PlaceholderSuggestionService } from '../../../../erp-common/suggestion/placeholder-suggestion.service';
 
 @Component({
   selector: 'jhi-invoice-update',
@@ -41,6 +43,16 @@ export class InvoiceUpdateComponent implements OnInit {
     placeholders: [],
   });
 
+  minAccountLengthTerm = 3;
+
+  labelsLoading = false;
+  labelControlInput$ = new Subject<string>();
+  labelLookups$: Observable<IPaymentLabel[]> = of([]);
+
+  placeholdersLoading = false;
+  placeholderControlInput$ = new Subject<string>();
+  placeholderLookups$: Observable<IPlaceholder[]> = of([]);
+
   constructor(
     protected invoiceService: InvoiceService,
     protected paymentLabelService: PaymentLabelService,
@@ -49,6 +61,8 @@ export class InvoiceUpdateComponent implements OnInit {
     protected fb: FormBuilder,
     protected store: Store<State>,
     protected router: Router,
+    protected labelSuggestionService: LabelSuggestionService,
+    protected placeholderSuggestionService: PlaceholderSuggestionService,
   ) {}
 
   ngOnInit(): void {
@@ -57,6 +71,56 @@ export class InvoiceUpdateComponent implements OnInit {
 
       this.loadRelationshipsOptions();
     });
+
+    // fire-up typeahead items
+    this.loadLabels();
+    this.loadPlaceholders();
+  }
+
+  loadLabels(): void {
+    this.labelLookups$ = concat(
+      of([]), // default items
+      this.labelControlInput$.pipe(
+        /* filter(res => res.length >= this.minAccountLengthTerm), */
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+        filter(res => res !== null),
+        distinctUntilChanged(),
+        debounceTime(800),
+        tap(() => this.labelsLoading = true),
+        switchMap(term => this.labelSuggestionService.search(term).pipe(
+          catchError(() => of([])),
+          tap(() => this.labelsLoading = false)
+        ))
+      ),
+      of([...this.paymentLabelsSharedCollection])
+    );
+  }
+
+  loadPlaceholders(): void {
+    this.placeholderLookups$ = concat(
+      of([]), // default items
+      this.placeholderControlInput$.pipe(
+        /* filter(res => res.length >= this.minAccountLengthTerm), */
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+        filter(res => res !== null),
+        distinctUntilChanged(),
+        debounceTime(800),
+        tap(() => this.placeholdersLoading = true),
+        switchMap(term => this.placeholderSuggestionService.search(term).pipe(
+          catchError(() => of([])),
+          tap(() => this.placeholdersLoading = false)
+        ))
+      ),
+      of([...this.placeholdersSharedCollection])
+    );
+  }
+
+  trackPlaceholdersByFn(item: IPaymentLabel): number {
+    return item.id!;
+  }
+
+  trackLabelByFn(item: IPaymentLabel): number {
+    return item.id!;
   }
 
   previousState(): void {
