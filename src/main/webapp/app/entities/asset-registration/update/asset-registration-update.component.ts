771231836d05705e85501252d8ca1039d2746208
Edwin Njeru <mailnjeru@gmail.com>
2022-05-02 10:36:54 +0300

    Creates prepayment-account and transaction-account entities

---
+++ src/main/webapp/app/entities/asset-registration/update/asset-registration-update.component.ts
@@ -0,0 +1,453 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { finalize, map } from 'rxjs/operators';
+
+import { IAssetRegistration, AssetRegistration } from '../asset-registration.model';
+import { AssetRegistrationService } from '../service/asset-registration.service';
+import { AlertError } from 'app/shared/alert/alert-error.model';
+import { EventManager, EventWithContent } from 'app/core/util/event-manager.service';
+import { DataUtils, FileLoadError } from 'app/core/util/data-util.service';
+import { IPlaceholder } from 'app/entities/erpService/placeholder/placeholder.model';
+import { PlaceholderService } from 'app/entities/erpService/placeholder/service/placeholder.service';
+import { IPaymentInvoice } from 'app/entities/payment-invoice/payment-invoice.model';
+import { PaymentInvoiceService } from 'app/entities/payment-invoice/service/payment-invoice.service';
+import { IServiceOutlet } from 'app/entities/service-outlet/service-outlet.model';
+import { ServiceOutletService } from 'app/entities/service-outlet/service/service-outlet.service';
+import { ISettlement } from 'app/entities/settlement/settlement.model';
+import { SettlementService } from 'app/entities/settlement/service/settlement.service';
+import { IAssetCategory } from 'app/entities/asset-category/asset-category.model';
+import { AssetCategoryService } from 'app/entities/asset-category/service/asset-category.service';
+import { IPurchaseOrder } from 'app/entities/purchase-order/purchase-order.model';
+import { PurchaseOrderService } from 'app/entities/purchase-order/service/purchase-order.service';
+import { IDeliveryNote } from 'app/entities/delivery-note/delivery-note.model';
+import { DeliveryNoteService } from 'app/entities/delivery-note/service/delivery-note.service';
+import { IJobSheet } from 'app/entities/job-sheet/job-sheet.model';
+import { JobSheetService } from 'app/entities/job-sheet/service/job-sheet.service';
+import { IDealer } from 'app/entities/dealers/dealer/dealer.model';
+import { DealerService } from 'app/entities/dealers/dealer/service/dealer.service';
+
+@Component({
+  selector: 'jhi-asset-registration-update',
+  templateUrl: './asset-registration-update.component.html',
+})
+export class AssetRegistrationUpdateComponent implements OnInit {
+  isSaving = false;
+
+  placeholdersSharedCollection: IPlaceholder[] = [];
+  paymentInvoicesSharedCollection: IPaymentInvoice[] = [];
+  serviceOutletsSharedCollection: IServiceOutlet[] = [];
+  settlementsSharedCollection: ISettlement[] = [];
+  assetCategoriesSharedCollection: IAssetCategory[] = [];
+  purchaseOrdersSharedCollection: IPurchaseOrder[] = [];
+  deliveryNotesSharedCollection: IDeliveryNote[] = [];
+  jobSheetsSharedCollection: IJobSheet[] = [];
+  dealersSharedCollection: IDealer[] = [];
+
+  editForm = this.fb.group({
+    id: [],
+    assetNumber: [null, [Validators.required]],
+    assetTag: [null, [Validators.required]],
+    assetDetails: [],
+    assetCost: [null, [Validators.required]],
+    comments: [],
+    commentsContentType: [],
+    placeholders: [],
+    paymentInvoices: [],
+    serviceOutlets: [null, Validators.required],
+    settlements: [null, Validators.required],
+    assetCategory: [null, Validators.required],
+    purchaseOrders: [],
+    deliveryNotes: [],
+    jobSheets: [],
+    dealer: [null, Validators.required],
+    designatedUsers: [],
+  });
+
+  constructor(
+    protected dataUtils: DataUtils,
+    protected eventManager: EventManager,
+    protected assetRegistrationService: AssetRegistrationService,
+    protected placeholderService: PlaceholderService,
+    protected paymentInvoiceService: PaymentInvoiceService,
+    protected serviceOutletService: ServiceOutletService,
+    protected settlementService: SettlementService,
+    protected assetCategoryService: AssetCategoryService,
+    protected purchaseOrderService: PurchaseOrderService,
+    protected deliveryNoteService: DeliveryNoteService,
+    protected jobSheetService: JobSheetService,
+    protected dealerService: DealerService,
+    protected activatedRoute: ActivatedRoute,
+    protected fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.data.subscribe(({ assetRegistration }) => {
+      this.updateForm(assetRegistration);
+
+      this.loadRelationshipsOptions();
+    });
+  }
+
+  byteSize(base64String: string): string {
+    return this.dataUtils.byteSize(base64String);
+  }
+
+  openFile(base64String: string, contentType: string | null | undefined): void {
+    this.dataUtils.openFile(base64String, contentType);
+  }
+
+  setFileData(event: Event, field: string, isImage: boolean): void {
+    this.dataUtils.loadFileToForm(event, this.editForm, field, isImage).subscribe({
+      error: (err: FileLoadError) =>
+        this.eventManager.broadcast(new EventWithContent<AlertError>('erpSystemApp.error', { message: err.message })),
+    });
+  }
+
+  previousState(): void {
+    window.history.back();
+  }
+
+  save(): void {
+    this.isSaving = true;
+    const assetRegistration = this.createFromForm();
+    if (assetRegistration.id !== undefined) {
+      this.subscribeToSaveResponse(this.assetRegistrationService.update(assetRegistration));
+    } else {
+      this.subscribeToSaveResponse(this.assetRegistrationService.create(assetRegistration));
+    }
+  }
+
+  trackPlaceholderById(index: number, item: IPlaceholder): number {
+    return item.id!;
+  }
+
+  trackPaymentInvoiceById(index: number, item: IPaymentInvoice): number {
+    return item.id!;
+  }
+
+  trackServiceOutletById(index: number, item: IServiceOutlet): number {
+    return item.id!;
+  }
+
+  trackSettlementById(index: number, item: ISettlement): number {
+    return item.id!;
+  }
+
+  trackAssetCategoryById(index: number, item: IAssetCategory): number {
+    return item.id!;
+  }
+
+  trackPurchaseOrderById(index: number, item: IPurchaseOrder): number {
+    return item.id!;
+  }
+
+  trackDeliveryNoteById(index: number, item: IDeliveryNote): number {
+    return item.id!;
+  }
+
+  trackJobSheetById(index: number, item: IJobSheet): number {
+    return item.id!;
+  }
+
+  trackDealerById(index: number, item: IDealer): number {
+    return item.id!;
+  }
+
+  getSelectedPlaceholder(option: IPlaceholder, selectedVals?: IPlaceholder[]): IPlaceholder {
+    if (selectedVals) {
+      for (const selectedVal of selectedVals) {
+        if (option.id === selectedVal.id) {
+          return selectedVal;
+        }
+      }
+    }
+    return option;
+  }
+
+  getSelectedPaymentInvoice(option: IPaymentInvoice, selectedVals?: IPaymentInvoice[]): IPaymentInvoice {
+    if (selectedVals) {
+      for (const selectedVal of selectedVals) {
+        if (option.id === selectedVal.id) {
+          return selectedVal;
+        }
+      }
+    }
+    return option;
+  }
+
+  getSelectedServiceOutlet(option: IServiceOutlet, selectedVals?: IServiceOutlet[]): IServiceOutlet {
+    if (selectedVals) {
+      for (const selectedVal of selectedVals) {
+        if (option.id === selectedVal.id) {
+          return selectedVal;
+        }
+      }
+    }
+    return option;
+  }
+
+  getSelectedSettlement(option: ISettlement, selectedVals?: ISettlement[]): ISettlement {
+    if (selectedVals) {
+      for (const selectedVal of selectedVals) {
+        if (option.id === selectedVal.id) {
+          return selectedVal;
+        }
+      }
+    }
+    return option;
+  }
+
+  getSelectedPurchaseOrder(option: IPurchaseOrder, selectedVals?: IPurchaseOrder[]): IPurchaseOrder {
+    if (selectedVals) {
+      for (const selectedVal of selectedVals) {
+        if (option.id === selectedVal.id) {
+          return selectedVal;
+        }
+      }
+    }
+    return option;
+  }
+
+  getSelectedDeliveryNote(option: IDeliveryNote, selectedVals?: IDeliveryNote[]): IDeliveryNote {
+    if (selectedVals) {
+      for (const selectedVal of selectedVals) {
+        if (option.id === selectedVal.id) {
+          return selectedVal;
+        }
+      }
+    }
+    return option;
+  }
+
+  getSelectedJobSheet(option: IJobSheet, selectedVals?: IJobSheet[]): IJobSheet {
+    if (selectedVals) {
+      for (const selectedVal of selectedVals) {
+        if (option.id === selectedVal.id) {
+          return selectedVal;
+        }
+      }
+    }
+    return option;
+  }
+
+  getSelectedDealer(option: IDealer, selectedVals?: IDealer[]): IDealer {
+    if (selectedVals) {
+      for (const selectedVal of selectedVals) {
+        if (option.id === selectedVal.id) {
+          return selectedVal;
+        }
+      }
+    }
+    return option;
+  }
+
+  protected subscribeToSaveResponse(result: Observable<HttpResponse<IAssetRegistration>>): void {
+    result.pipe(finalize(() => this.onSaveFinalize())).subscribe(
+      () => this.onSaveSuccess(),
+      () => this.onSaveError()
+    );
+  }
+
+  protected onSaveSuccess(): void {
+    this.previousState();
+  }
+
+  protected onSaveError(): void {
+    // Api for inheritance.
+  }
+
+  protected onSaveFinalize(): void {
+    this.isSaving = false;
+  }
+
+  protected updateForm(assetRegistration: IAssetRegistration): void {
+    this.editForm.patchValue({
+      id: assetRegistration.id,
+      assetNumber: assetRegistration.assetNumber,
+      assetTag: assetRegistration.assetTag,
+      assetDetails: assetRegistration.assetDetails,
+      assetCost: assetRegistration.assetCost,
+      comments: assetRegistration.comments,
+      commentsContentType: assetRegistration.commentsContentType,
+      placeholders: assetRegistration.placeholders,
+      paymentInvoices: assetRegistration.paymentInvoices,
+      serviceOutlets: assetRegistration.serviceOutlets,
+      settlements: assetRegistration.settlements,
+      assetCategory: assetRegistration.assetCategory,
+      purchaseOrders: assetRegistration.purchaseOrders,
+      deliveryNotes: assetRegistration.deliveryNotes,
+      jobSheets: assetRegistration.jobSheets,
+      dealer: assetRegistration.dealer,
+      designatedUsers: assetRegistration.designatedUsers,
+    });
+
+    this.placeholdersSharedCollection = this.placeholderService.addPlaceholderToCollectionIfMissing(
+      this.placeholdersSharedCollection,
+      ...(assetRegistration.placeholders ?? [])
+    );
+    this.paymentInvoicesSharedCollection = this.paymentInvoiceService.addPaymentInvoiceToCollectionIfMissing(
+      this.paymentInvoicesSharedCollection,
+      ...(assetRegistration.paymentInvoices ?? [])
+    );
+    this.serviceOutletsSharedCollection = this.serviceOutletService.addServiceOutletToCollectionIfMissing(
+      this.serviceOutletsSharedCollection,
+      ...(assetRegistration.serviceOutlets ?? [])
+    );
+    this.settlementsSharedCollection = this.settlementService.addSettlementToCollectionIfMissing(
+      this.settlementsSharedCollection,
+      ...(assetRegistration.settlements ?? [])
+    );
+    this.assetCategoriesSharedCollection = this.assetCategoryService.addAssetCategoryToCollectionIfMissing(
+      this.assetCategoriesSharedCollection,
+      assetRegistration.assetCategory
+    );
+    this.purchaseOrdersSharedCollection = this.purchaseOrderService.addPurchaseOrderToCollectionIfMissing(
+      this.purchaseOrdersSharedCollection,
+      ...(assetRegistration.purchaseOrders ?? [])
+    );
+    this.deliveryNotesSharedCollection = this.deliveryNoteService.addDeliveryNoteToCollectionIfMissing(
+      this.deliveryNotesSharedCollection,
+      ...(assetRegistration.deliveryNotes ?? [])
+    );
+    this.jobSheetsSharedCollection = this.jobSheetService.addJobSheetToCollectionIfMissing(
+      this.jobSheetsSharedCollection,
+      ...(assetRegistration.jobSheets ?? [])
+    );
+    this.dealersSharedCollection = this.dealerService.addDealerToCollectionIfMissing(
+      this.dealersSharedCollection,
+      assetRegistration.dealer,
+      ...(assetRegistration.designatedUsers ?? [])
+    );
+  }
+
+  protected loadRelationshipsOptions(): void {
+    this.placeholderService
+      .query()
+      .pipe(map((res: HttpResponse<IPlaceholder[]>) => res.body ?? []))
+      .pipe(
+        map((placeholders: IPlaceholder[]) =>
+          this.placeholderService.addPlaceholderToCollectionIfMissing(placeholders, ...(this.editForm.get('placeholders')!.value ?? []))
+        )
+      )
+      .subscribe((placeholders: IPlaceholder[]) => (this.placeholdersSharedCollection = placeholders));
+
+    this.paymentInvoiceService
+      .query()
+      .pipe(map((res: HttpResponse<IPaymentInvoice[]>) => res.body ?? []))
+      .pipe(
+        map((paymentInvoices: IPaymentInvoice[]) =>
+          this.paymentInvoiceService.addPaymentInvoiceToCollectionIfMissing(
+            paymentInvoices,
+            ...(this.editForm.get('paymentInvoices')!.value ?? [])
+          )
+        )
+      )
+      .subscribe((paymentInvoices: IPaymentInvoice[]) => (this.paymentInvoicesSharedCollection = paymentInvoices));
+
+    this.serviceOutletService
+      .query()
+      .pipe(map((res: HttpResponse<IServiceOutlet[]>) => res.body ?? []))
+      .pipe(
+        map((serviceOutlets: IServiceOutlet[]) =>
+          this.serviceOutletService.addServiceOutletToCollectionIfMissing(
+            serviceOutlets,
+            ...(this.editForm.get('serviceOutlets')!.value ?? [])
+          )
+        )
+      )
+      .subscribe((serviceOutlets: IServiceOutlet[]) => (this.serviceOutletsSharedCollection = serviceOutlets));
+
+    this.settlementService
+      .query()
+      .pipe(map((res: HttpResponse<ISettlement[]>) => res.body ?? []))
+      .pipe(
+        map((settlements: ISettlement[]) =>
+          this.settlementService.addSettlementToCollectionIfMissing(settlements, ...(this.editForm.get('settlements')!.value ?? []))
+        )
+      )
+      .subscribe((settlements: ISettlement[]) => (this.settlementsSharedCollection = settlements));
+
+    this.assetCategoryService
+      .query()
+      .pipe(map((res: HttpResponse<IAssetCategory[]>) => res.body ?? []))
+      .pipe(
+        map((assetCategories: IAssetCategory[]) =>
+          this.assetCategoryService.addAssetCategoryToCollectionIfMissing(assetCategories, this.editForm.get('assetCategory')!.value)
+        )
+      )
+      .subscribe((assetCategories: IAssetCategory[]) => (this.assetCategoriesSharedCollection = assetCategories));
+
+    this.purchaseOrderService
+      .query()
+      .pipe(map((res: HttpResponse<IPurchaseOrder[]>) => res.body ?? []))
+      .pipe(
+        map((purchaseOrders: IPurchaseOrder[]) =>
+          this.purchaseOrderService.addPurchaseOrderToCollectionIfMissing(
+            purchaseOrders,
+            ...(this.editForm.get('purchaseOrders')!.value ?? [])
+          )
+        )
+      )
+      .subscribe((purchaseOrders: IPurchaseOrder[]) => (this.purchaseOrdersSharedCollection = purchaseOrders));
+
+    this.deliveryNoteService
+      .query()
+      .pipe(map((res: HttpResponse<IDeliveryNote[]>) => res.body ?? []))
+      .pipe(
+        map((deliveryNotes: IDeliveryNote[]) =>
+          this.deliveryNoteService.addDeliveryNoteToCollectionIfMissing(deliveryNotes, ...(this.editForm.get('deliveryNotes')!.value ?? []))
+        )
+      )
+      .subscribe((deliveryNotes: IDeliveryNote[]) => (this.deliveryNotesSharedCollection = deliveryNotes));
+
+    this.jobSheetService
+      .query()
+      .pipe(map((res: HttpResponse<IJobSheet[]>) => res.body ?? []))
+      .pipe(
+        map((jobSheets: IJobSheet[]) =>
+          this.jobSheetService.addJobSheetToCollectionIfMissing(jobSheets, ...(this.editForm.get('jobSheets')!.value ?? []))
+        )
+      )
+      .subscribe((jobSheets: IJobSheet[]) => (this.jobSheetsSharedCollection = jobSheets));
+
+    this.dealerService
+      .query()
+      .pipe(map((res: HttpResponse<IDealer[]>) => res.body ?? []))
+      .pipe(
+        map((dealers: IDealer[]) =>
+          this.dealerService.addDealerToCollectionIfMissing(
+            dealers,
+            this.editForm.get('dealer')!.value,
+            ...(this.editForm.get('designatedUsers')!.value ?? [])
+          )
+        )
+      )
+      .subscribe((dealers: IDealer[]) => (this.dealersSharedCollection = dealers));
+  }
+
+  protected createFromForm(): IAssetRegistration {
+    return {
+      ...new AssetRegistration(),
+      id: this.editForm.get(['id'])!.value,
+      assetNumber: this.editForm.get(['assetNumber'])!.value,
+      assetTag: this.editForm.get(['assetTag'])!.value,
+      assetDetails: this.editForm.get(['assetDetails'])!.value,
+      assetCost: this.editForm.get(['assetCost'])!.value,
+      commentsContentType: this.editForm.get(['commentsContentType'])!.value,
+      comments: this.editForm.get(['comments'])!.value,
+      placeholders: this.editForm.get(['placeholders'])!.value,
+      paymentInvoices: this.editForm.get(['paymentInvoices'])!.value,
+      serviceOutlets: this.editForm.get(['serviceOutlets'])!.value,
+      settlements: this.editForm.get(['settlements'])!.value,
+      assetCategory: this.editForm.get(['assetCategory'])!.value,
+      purchaseOrders: this.editForm.get(['purchaseOrders'])!.value,
+      deliveryNotes: this.editForm.get(['deliveryNotes'])!.value,
+      jobSheets: this.editForm.get(['jobSheets'])!.value,
+      dealer: this.editForm.get(['dealer'])!.value,
+      designatedUsers: this.editForm.get(['designatedUsers'])!.value,
+    };
+  }
+}
---
+++ src/test/javascript/e2e/entities/prepayment-account/prepayment-account.page-object.ts
@@ -0,0 +1,217 @@
+import { element, by, ElementFinder } from 'protractor';
+
+export class PrepaymentAccountComponentsPage {
+  createButton = element(by.id('jh-create-entity'));
+  deleteButtons = element.all(by.css('jhi-prepayment-account div table .btn-danger'));
+  title = element.all(by.css('jhi-prepayment-account div h2#page-heading span')).first();
+  noResult = element(by.id('no-result'));
+  entities = element(by.id('entities'));
+
+  async clickOnCreateButton(): Promise<void> {
+    await this.createButton.click();
+  }
+
+  async clickOnLastDeleteButton(): Promise<void> {
+    await this.deleteButtons.last().click();
+  }
+
+  async countDeleteButtons(): Promise<number> {
+    return this.deleteButtons.count();
+  }
+
+  async getTitle(): Promise<string> {
+    return this.title.getText();
+  }
+}
+
+export class PrepaymentAccountUpdatePage {
+  pageTitle = element(by.id('jhi-prepayment-account-heading'));
+  saveButton = element(by.id('save-entity'));
+  cancelButton = element(by.id('cancel-save'));
+
+  idInput = element(by.id('field_id'));
+  catalogueNumberInput = element(by.id('field_catalogueNumber'));
+  particularsInput = element(by.id('field_particulars'));
+  notesInput = element(by.id('field_notes'));
+
+  settlementCurrencySelect = element(by.id('field_settlementCurrency'));
+  prepaymentTransactionSelect = element(by.id('field_prepaymentTransaction'));
+  serviceOutletSelect = element(by.id('field_serviceOutlet'));
+  dealerSelect = element(by.id('field_dealer'));
+  placeholderSelect = element(by.id('field_placeholder'));
+  debitAccountSelect = element(by.id('field_debitAccount'));
+  transferAccountSelect = element(by.id('field_transferAccount'));
+
+  async getPageTitle(): Promise<string> {
+    return this.pageTitle.getText();
+  }
+
+  async setIdInput(id: string): Promise<void> {
+    await this.idInput.sendKeys(id);
+  }
+
+  async getIdInput(): Promise<string> {
+    return await this.idInput.getAttribute('value');
+  }
+
+  async setCatalogueNumberInput(catalogueNumber: string): Promise<void> {
+    await this.catalogueNumberInput.sendKeys(catalogueNumber);
+  }
+
+  async getCatalogueNumberInput(): Promise<string> {
+    return await this.catalogueNumberInput.getAttribute('value');
+  }
+
+  async setParticularsInput(particulars: string): Promise<void> {
+    await this.particularsInput.sendKeys(particulars);
+  }
+
+  async getParticularsInput(): Promise<string> {
+    return await this.particularsInput.getAttribute('value');
+  }
+
+  async setNotesInput(notes: string): Promise<void> {
+    await this.notesInput.sendKeys(notes);
+  }
+
+  async getNotesInput(): Promise<string> {
+    return await this.notesInput.getAttribute('value');
+  }
+
+  async settlementCurrencySelectLastOption(): Promise<void> {
+    await this.settlementCurrencySelect.all(by.tagName('option')).last().click();
+  }
+
+  async settlementCurrencySelectOption(option: string): Promise<void> {
+    await this.settlementCurrencySelect.sendKeys(option);
+  }
+
+  getSettlementCurrencySelect(): ElementFinder {
+    return this.settlementCurrencySelect;
+  }
+
+  async getSettlementCurrencySelectedOption(): Promise<string> {
+    return await this.settlementCurrencySelect.element(by.css('option:checked')).getText();
+  }
+
+  async prepaymentTransactionSelectLastOption(): Promise<void> {
+    await this.prepaymentTransactionSelect.all(by.tagName('option')).last().click();
+  }
+
+  async prepaymentTransactionSelectOption(option: string): Promise<void> {
+    await this.prepaymentTransactionSelect.sendKeys(option);
+  }
+
+  getPrepaymentTransactionSelect(): ElementFinder {
+    return this.prepaymentTransactionSelect;
+  }
+
+  async getPrepaymentTransactionSelectedOption(): Promise<string> {
+    return await this.prepaymentTransactionSelect.element(by.css('option:checked')).getText();
+  }
+
+  async serviceOutletSelectLastOption(): Promise<void> {
+    await this.serviceOutletSelect.all(by.tagName('option')).last().click();
+  }
+
+  async serviceOutletSelectOption(option: string): Promise<void> {
+    await this.serviceOutletSelect.sendKeys(option);
+  }
+
+  getServiceOutletSelect(): ElementFinder {
+    return this.serviceOutletSelect;
+  }
+
+  async getServiceOutletSelectedOption(): Promise<string> {
+    return await this.serviceOutletSelect.element(by.css('option:checked')).getText();
+  }
+
+  async dealerSelectLastOption(): Promise<void> {
+    await this.dealerSelect.all(by.tagName('option')).last().click();
+  }
+
+  async dealerSelectOption(option: string): Promise<void> {
+    await this.dealerSelect.sendKeys(option);
+  }
+
+  getDealerSelect(): ElementFinder {
+    return this.dealerSelect;
+  }
+
+  async getDealerSelectedOption(): Promise<string> {
+    return await this.dealerSelect.element(by.css('option:checked')).getText();
+  }
+
+  async placeholderSelectLastOption(): Promise<void> {
+    await this.placeholderSelect.all(by.tagName('option')).last().click();
+  }
+
+  async placeholderSelectOption(option: string): Promise<void> {
+    await this.placeholderSelect.sendKeys(option);
+  }
+
+  getPlaceholderSelect(): ElementFinder {
+    return this.placeholderSelect;
+  }
+
+  async getPlaceholderSelectedOption(): Promise<string> {
+    return await this.placeholderSelect.element(by.css('option:checked')).getText();
+  }
+
+  async debitAccountSelectLastOption(): Promise<void> {
+    await this.debitAccountSelect.all(by.tagName('option')).last().click();
+  }
+
+  async debitAccountSelectOption(option: string): Promise<void> {
+    await this.debitAccountSelect.sendKeys(option);
+  }
+
+  getDebitAccountSelect(): ElementFinder {
+    return this.debitAccountSelect;
+  }
+
+  async getDebitAccountSelectedOption(): Promise<string> {
+    return await this.debitAccountSelect.element(by.css('option:checked')).getText();
+  }
+
+  async transferAccountSelectLastOption(): Promise<void> {
+    await this.transferAccountSelect.all(by.tagName('option')).last().click();
+  }
+
+  async transferAccountSelectOption(option: string): Promise<void> {
+    await this.transferAccountSelect.sendKeys(option);
+  }
+
+  getTransferAccountSelect(): ElementFinder {
+    return this.transferAccountSelect;
+  }
+
+  async getTransferAccountSelectedOption(): Promise<string> {
+    return await this.transferAccountSelect.element(by.css('option:checked')).getText();
+  }
+
+  async save(): Promise<void> {
+    await this.saveButton.click();
+  }
+
+  async cancel(): Promise<void> {
+    await this.cancelButton.click();
+  }
+
+  getSaveButton(): ElementFinder {
+    return this.saveButton;
+  }
+}
+
+export class PrepaymentAccountDeleteDialog {
+  private dialogTitle = element(by.id('jhi-delete-prepaymentAccount-heading'));
+  private confirmButton = element(by.id('jhi-confirm-delete-prepaymentAccount'));
+
+  async getDialogTitle(): Promise<string> {
+    return this.dialogTitle.getText();
+  }
+
+  async clickOnConfirmButton(): Promise<void> {
+    await this.confirmButton.click();
+  }
+}
