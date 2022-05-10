4c6d80d1ab615d3846c1d70fc1c9f6138da26611
Edwin Njeru <mailnjeru@gmail.com>
2022-05-10 19:23:02 +0300

    Removes dead code from the m21 dealer component

--- src/main/webapp/app/erp/erp-common/services/dealer.service.ts
+++ src/main/webapp/app/erp/erp-common/services/dealer.service.ts
@@ -6,7 +6,6 @@ import { isPresent } from 'app/core/util/operators';
 import { ApplicationConfigService } from 'app/core/config/application-config.service';
 import { createRequestOption } from 'app/core/request/request-util';
 import { SearchWithPagination } from 'app/core/request/request.model';
-import { ErpCommonModule } from '../erp-common.module';
 import { getDealerIdentifier, IDealer } from '../models/dealer.model';
 
 export type EntityResponseType = HttpResponse<IDealer>;
