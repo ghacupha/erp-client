dcc50e697998dd2f1911939644ae5b62e5906f00
Edwin Njeru <mailnjeru@gmail.com>
2022-05-10 16:40:53 +0300

    Removes dead code from the m2m dealer component

--- src/main/webapp/app/erp/erp-common/form-components/dealer-form-controls/m2m-dealer-form-control.component.ts
+++ src/main/webapp/app/erp/erp-common/form-components/dealer-form-controls/m2m-dealer-form-control.component.ts
@@ -1,10 +1,9 @@
-import { Component, EventEmitter, forwardRef, HostBinding, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 import { IDealer } from '../../../../entities/dealers/dealer/dealer.model';
 import { concat, Observable, of, Subject } from 'rxjs';
 import { DealerService } from '../../../../entities/dealers/dealer/service/dealer.service';
-import { catchError, debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs/operators';
-import { HttpResponse } from '@angular/common/http';
+import { catchError, debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs/operators';
 import { DealerInputControlService } from './dealer-input-control.service';
 
 @Component({
@@ -24,22 +23,13 @@ export class M2MDealerFormControlComponent implements OnInit, ControlValueAccess
   // TODO Create similar components for other entities
   // TODO Add self validation code via NG_VALIDATOR provider
 
-  @HostBinding('attr.id') externalId = '';
-
   @Input() inputDealers: IDealer[] = [];
 
   @Input() inputControlLabel = '';
 
   @Output() dealerSelected: EventEmitter<IDealer[]> = new EventEmitter<IDealer[]>();
 
-  selectedDealer: IDealer = {};
-
-  isSaving = false;
-
-  dealersSharedCollection: IDealer[] = [];
-
   minAccountLengthTerm = 3;
-
   dealersLoading = false;
   dealersInput$ = new Subject<string>();
   dealerLookups$: Observable<IDealer[]> = of([]);
@@ -57,11 +47,6 @@ export class M2MDealerFormControlComponent implements OnInit, ControlValueAccess
   onTouched: any = () => {};
 
   ngOnInit(): void {
-
-    this.updateForm();
-
-    this.loadRelationshipsOptions();
-
     this.loadDealers();
   }
 
@@ -79,8 +64,7 @@ export class M2MDealerFormControlComponent implements OnInit, ControlValueAccess
           catchError(() => of([])),
           tap(() => this.dealersLoading = false)
         ))
-      ),
-      of([])
+      )
     );
   }
 
@@ -88,43 +72,28 @@ export class M2MDealerFormControlComponent implements OnInit, ControlValueAccess
     return item.id!;
   }
 
+  /**
+   * Replaces the array received from the parent with the one currently in view
+   *
+   * @param value
+   */
   writeValue(value: IDealer[]): void {
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    if (value) {
+    if (value.length !== 0) {
       this.inputDealers = value
     }
   }
 
+  /**
+   * Emits updated array to parent
+   */
   getValues(): void {
-    // eslint-disable-next-line no-console
-    console.log(`Picking changed values: ${this.selectedDealer}`);
     this.dealerSelected.emit(this.inputDealers);
   }
 
-  previousState(): void {
-    window.history.back();
-  }
-
-  protected updateForm(): void {
-    this.dealersSharedCollection = this.dealerService.addDealerToCollectionIfMissing(
-      this.inputDealers
-    );
-  }
-
-  protected loadRelationshipsOptions(): void {
-    this.dealerService
-      .query()
-      .pipe(map((res: HttpResponse<IDealer[]>) => res.body ?? []))
-      .pipe(map((dealers: IDealer[]) => this.dealerService.addDealerToCollectionIfMissing(dealers)))
-      .subscribe((dealers: IDealer[]) => (this.dealersSharedCollection = dealers));
-  }
-
-  // eslint-disable-next-line @typescript-eslint/member-ordering,@typescript-eslint/no-empty-function
   registerOnChange(fn: any): void {
     this.onChange = fn;
   }
 
-  // eslint-disable-next-line @typescript-eslint/member-ordering,@typescript-eslint/no-empty-function
   registerOnTouched(fn: any): void {
     this.onTouched = fn;
   }
