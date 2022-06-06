36a178c40796dadce1e1a6e4f3f5359ca8d0fc6e
Edwin Njeru <mailnjeru@gmail.com>
2022-06-06 17:56:41 +0300

    Additional container configurations

--- webpack/environment.js
+++ webpack/environment.js
@@ -1,6 +1,6 @@
 module.exports = {
   I18N_HASH: 'generated_hash',
-  SERVER_API_URL: `'http://localhost:8980/'`,
+  SERVER_API_URL: '',
   __VERSION__: process.env.hasOwnProperty('APP_VERSION') ? process.env.APP_VERSION : 'DEV',
   __DEBUG_INFO_ENABLED__: false,
 };
