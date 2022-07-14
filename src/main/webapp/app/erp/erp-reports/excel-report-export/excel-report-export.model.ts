f7e4a9e285b648eee9e4c3f0d48d43fa9d30c9dc
Edwin Njeru <mailnjeru@gmail.com>
2022-07-14 16:08:07 +0300

    Update the milestone series to match the server's

--- src/main/webapp/app/erp/erp-reports/excel-report-export/excel-report-export.model.ts
+++ src/main/webapp/app/erp/erp-reports/excel-report-export/excel-report-export.model.ts
@@ -1,3 +1,21 @@
+///
+/// Erp System - Mark II No 19 (Baruch Series)
+/// Copyright © 2021 - 2022 Edwin Njeru (mailnjeru@gmail.com)
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///
+
 import * as dayjs from 'dayjs';
 import { IPlaceholder } from '../../erp-pages/placeholder/placeholder.model';
 import { IUniversallyUniqueMapping } from '../../erp-pages/universally-unique-mapping/universally-unique-mapping.model';
