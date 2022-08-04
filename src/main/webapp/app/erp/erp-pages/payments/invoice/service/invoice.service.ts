2f8b35166e3e5be35b92c3ccfe815cf96df95c4a
Edwin Njeru <mailnjeru@gmail.com>
2022-08-04 14:30:58 +0300

    Add missing license headers

--- src/main/webapp/app/erp/erp-pages/payments/invoice/service/invoice.service.ts
+++ src/main/webapp/app/erp/erp-pages/payments/invoice/service/invoice.service.ts
@@ -1,3 +1,21 @@
+///
+/// Erp System - Mark II No 23 (Baruch Series) Client v 0.1.1-SNAPSHOT
+/// Copyright © 2021 - 2022 Edwin Njeru (mailnjeru@gmail.com)
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///
+
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import {Observable, of} from 'rxjs';
@@ -15,7 +33,6 @@ import {
   dealerInvoiceSelectedPayment
 } from "../../../../store/selectors/dealer-invoice-worklows-status.selectors";
 import {NGXLogger} from "ngx-logger";
-import { ErpCommonModule } from '../../../../erp-common/erp-common.module';
 import { getInvoiceIdentifier, IInvoice, Invoice } from '../invoice.model';
 import { IPayment, Payment } from '../../payment/payment.model';
 
