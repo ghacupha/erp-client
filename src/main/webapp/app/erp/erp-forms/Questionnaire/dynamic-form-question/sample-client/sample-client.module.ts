8471e9eaab78bed1306b1b660ac1b25d345c7731
Edwin Njeru <mailnjeru@gmail.com>
2022-11-08 16:28:32 +0300

    Test #1 dynamic-forms complete

--- src/main/webapp/app/erp/erp-forms/Questionnaire/dynamic-form-question/sample-client/sample-client.module.ts
+++ src/main/webapp/app/erp/erp-forms/Questionnaire/dynamic-form-question/sample-client/sample-client.module.ts
@@ -4,13 +4,16 @@ import { DynamicFormModule } from '../dynamic-form/dynamic-form.module';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../../../../../shared/shared.module';
 
+/**
+ * This test uses the base angular tutorial model for creating dynamic form
+ */
 @NgModule({
   declarations: [SampleClientComponent],
   imports: [
     DynamicFormModule,
     RouterModule.forRoot([
       {
-        path: 'test/dynamic-form',
+        path: 'test/dynamic-form/test#1',
         data: { pageTitle: 'ERP | Dynamic Form Test' },
         component: SampleClientComponent
       }
