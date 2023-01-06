423cb1dec5eb05fccfe889e0331eebf3e9bcbd81
Edwin Njeru <mailnjeru@gmail.com>
2023-01-06 15:31:11 +0300

    Increase the max file upload size

--- src/main/docker/docker-defaults.sh
+++ src/main/docker/docker-defaults.sh
@@ -30,6 +30,7 @@ set -eu
 # to set the defaults:
 export SERVER_API_DOCKER_DEPLOY_HOST=${SERVER_API_DOCKER_DEPLOY_HOST:-http://localhost}
 export SERVER_API_DOCKER_DEPLOY_PORT=${SERVER_API_DOCKER_DEPLOY_PORT:-8980}
+export ERP_DOCUMENTS_MAX_FILE_SIZE=${ERP_DOCUMENTS_MAX_FILE_SIZE:-50M}
 
 # Due to `set -u` this would fail if not defined and no default was set above
 echo "Requests proxy configured for /* to ${SERVER_API_DOCKER_DEPLOY_HOST}:${SERVER_API_DOCKER_DEPLOY_PORT} /*"
