3b963ff227b63b18590e316d4f17a5ab4d67cda7
Edwin Njeru <mailnjeru@gmail.com>
2023-02-28 11:39:03 +0300

    Releasing v1.1.1-SNAPSHOT

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -15,6 +15,7 @@
 /// You should have received a copy of the GNU General Public License
 /// along with this program. If not, see <http://www.gnu.org/licenses/>.
 ///
+
 // noinspection ES6UnusedImports
 
 import { NgModule, LOCALE_ID } from '@angular/core';
