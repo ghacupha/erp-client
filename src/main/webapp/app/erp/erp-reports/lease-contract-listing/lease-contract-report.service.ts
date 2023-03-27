cf991ecd79c7ad61cdbf3b82b3d9224f995bc8e4
Edwin Njeru <mailnjeru@gmail.com>
2023-03-27 15:02:53 +0300

    Sample lease reports

---
+++ src/main/webapp/app/erp/erp-reports/lease-contract-listing/lease-contract-report.service.ts
@@ -0,0 +1,47 @@
+///
+/// Erp System - Mark III No 11 (Caleb Series) Client 1.1.1
+/// Copyright © 2021 - 2023 Edwin Njeru (mailnjeru@gmail.com)
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///
+
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import * as dayjs from 'dayjs';
+import { map } from 'rxjs/operators';
+import { ILeaseContract, ILeaseContractQuery } from './lease-contract-report.model';
+
+type EntityArrayResponseType = HttpResponse<ILeaseContract[]>;
+
+@Injectable({ providedIn: 'root' })
+export class LeaseContractReportService {
+  public resourceUrl = SERVER_API_URL + 'api/leases/contract-listing';
+
+  constructor(protected http: HttpClient) {}
+
+  query(reportQuery: ILeaseContractQuery): Observable<EntityArrayResponseType> {
+    return this.http.post<ILeaseContract[]>(this.resourceUrl, reportQuery, { observe: 'response' })
+      .pipe(map((res: EntityArrayResponseType) => this.convertDateArrayFromServer(res)));
+  }
+
+  protected convertDateArrayFromServer(res: EntityArrayResponseType): EntityArrayResponseType {
+    if (res.body) {
+      res.body.forEach((listItem: ILeaseContract) => {
+        listItem.timeOfRequisition = listItem.timeOfRequisition ? dayjs(listItem.timeOfRequisition): undefined;
+      });
+    }
+    return res;
+  }
+}
---
+++ src/main/webapp/app/erp/erp-reports/lease-contract-listing/lease-contract-reports.module.ts
@@ -0,0 +1,47 @@
+///
+/// Erp System - Mark III No 11 (Caleb Series) Client 1.1.1
+/// Copyright © 2021 - 2023 Edwin Njeru (mailnjeru@gmail.com)
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DataTablesModule as DTModule } from 'angular-datatables';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../../../shared/shared.module';
+import { LEASE_CONTRACT_REPORT_ROUTE } from './lease-contract-reports.route';
+import { LeaseContractReportComponent } from './lease-contract-report.component';
+
+const routes: Routes = [LEASE_CONTRACT_REPORT_ROUTE];
+
+/**
+ * Module for ledger-dt-view that uses datatables library to view ledgers in excel
+ * exportable format on the web view
+ */
+@NgModule({
+  declarations: [LeaseContractReportComponent],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule,
+    SharedModule,
+    DTModule
+  ],
+  exports: [
+    RouterModule,
+    LeaseContractReportComponent
+  ],
+  entryComponents: [LeaseContractReportComponent],
+})
+export class LeaseContractReportsModule {}
