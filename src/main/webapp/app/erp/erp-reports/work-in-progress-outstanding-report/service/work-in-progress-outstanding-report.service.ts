351e3daeb55f5c18d540ca4cfeff2707a3a480af
Edwin Njeru <mailnjeru@gmail.com>
2023-10-31 18:49:40 +0300

    Implement wip outstanding report entity

--- src/main/webapp/app/erp/erp-reports/work-in-progress-outstanding-report/service/work-in-progress-outstanding-report.service.ts
+++ src/main/webapp/app/erp/erp-reports/work-in-progress-outstanding-report/service/work-in-progress-outstanding-report.service.ts
@@ -17,7 +17,7 @@
 ///
 
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { isPresent } from 'app/core/util/operators';
@@ -34,13 +34,20 @@ export type EntityArrayResponseType = HttpResponse<IWorkInProgressOutstandingRep
 
 @Injectable({ providedIn: 'root' })
 export class WorkInProgressOutstandingReportService {
-  protected resourceUrl = this.applicationConfigService.getEndpointFor('api/work-in-progress-outstanding-reports');
-  protected resourceSearchUrl = this.applicationConfigService.getEndpointFor('api/_search/work-in-progress-outstanding-reports');
+  protected resourceUrl = this.applicationConfigService.getEndpointFor('api/fixed-asset/work-in-progress-outstanding-reports');
+  protected resourceSearchUrl = this.applicationConfigService.getEndpointFor('api/fixed-asset/_search/work-in-progress-outstanding-reports');
 
   constructor(protected http: HttpClient, protected applicationConfigService: ApplicationConfigService) {}
 
   find(id: number): Observable<EntityResponseType> {
     return this.http.get<IWorkInProgressOutstandingReport>(`${this.resourceUrl}/${id}`, { observe: 'response' });
+  }
+
+  queryByReportDate(reportDate: string, req?: any): Observable<EntityArrayResponseType> {
+    const options = createRequestOption(req);
+    options.set('reportDate', reportDate)
+
+    return this.http.get<IWorkInProgressOutstandingReport[]>(this.resourceUrl + `/reported`, { params: options, observe: 'response' });
   }
 
   query(req?: any): Observable<EntityArrayResponseType> {
