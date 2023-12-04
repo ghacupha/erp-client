1f17d133e15ab5951ef2355a78697f44a7457ed2
Edwin Njeru <mailnjeru@gmail.com>
2023-12-04 15:17:59 +0300

    Update marshalling components

--- src/main/webapp/app/erp/erp-common/pipe/format-fiscal-month.pipe.ts
+++ src/main/webapp/app/erp/erp-common/pipe/format-fiscal-month.pipe.ts
@@ -24,7 +24,7 @@ import { IFiscalMonth } from '../../erp-pages/fiscal-month/fiscal-month.model';
 })
 export class FormatFiscalMonthPipe implements PipeTransform {
 
-  transform(value: IFiscalMonth, args: any[]): string {
+  transform(value: IFiscalMonth): string {
 
     let detail = '';
 
@@ -32,16 +32,8 @@ export class FormatFiscalMonthPipe implements PipeTransform {
     // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
     if (value) {
 
-      const limit = args.length > 0 ? parseInt(args[0], 10) : 30;
-      const trail = args.length > 1 ? args[1] : '...';
-
-      // eslint-disable-next-line @typescript-eslint/restrict-plus-operands,@typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
-      const desc = value.description!.length > limit ? value.description.substing(0, limit) + trail : value.description;
-
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-        detail = `Code: ${value.fiscalMonthCode} | Start Date: ${value.startDate} | End Date: ${value.endDate} |${desc}`;
+        detail = `Id: ${value.id} | Start Date: ${value.startDate} | End Date: ${value.endDate} | ${value.fiscalMonthCode}`;
     }
 
     // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
