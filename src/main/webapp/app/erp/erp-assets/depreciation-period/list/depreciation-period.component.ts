8c432d52483a10f51012a7f52cf8dcd43989895d
Edwin Njeru <mailnjeru@gmail.com>
2024-04-26 09:35:19 +0300

    Make period list to 20 items

--- src/main/webapp/app/erp/erp-assets/depreciation-period/list/depreciation-period.component.ts
+++ src/main/webapp/app/erp/erp-assets/depreciation-period/list/depreciation-period.component.ts
@@ -37,7 +37,7 @@ export class DepreciationPeriodComponent implements OnInit {
   currentSearch: string;
   isLoading = false;
   totalItems = 0;
-  itemsPerPage = ITEMS_PER_PAGE;
+  itemsPerPage = 20;
   page?: number;
   predicate!: string;
   ascending!: boolean;
